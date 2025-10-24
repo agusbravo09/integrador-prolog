77704f0e35099b28b96b83106c9f45c1b2a9e90d
Sofia <guardiasofia@gmail.com>
2025-10-24 17:50:20 -0300

    Menu explicaciones creado, consultas de vida compleja y vida inteligente

--- consultas.pl
+++ consultas.pl
@@ -18,12 +18,33 @@ consultas(1) :- writeln('*** Planetas con vida basica ***'),
                     nl, menu2.
 
 
-consultas(2) :- write('seleccionaste la consulta 2'), nl, menu2.
-consultas(3) :- write('seleccionaste la consulta 3'), nl, menu2.
+consultas(2) :- writeln('*** Planetas con vida compleja ***'),
+                (inferir(vida_compleja, _) ->
+                    forall(inferir(vida_compleja, Planeta), format('~w~n', [Planeta]))
+                    ;
+                    writeln('No se encontraron planetas con vida compleja...')),
+                nl, menu2.
+
+consultas(3) :- writeln('*** Planetas con vida inteligente ***'),
+                (inferir(vida_inteligente, _) ->
+                    forall(inferir(vida_inteligente, Planeta), format('~w~n', [Planeta]))
+                    ;
+                    writeln('No se encontraron planetas con vida inteligente...')),
+                nl, menu2.
+                
 consultas(4) :- write('seleccionaste la consulta 4'), nl, menu2.
 consultas(5) :- write('seleccionaste la consulta 5'), nl, menu2.
 consultas(_) :- write('Volviendo al inicio...'), nl, menu.
 
 
+menu_explicaciones :-
+    write('*** Menu de Explicaciones ***'), nl,
+    write('1. Explicar vida basica de un planeta'), nl,
+    write('2. Explicar vida compleja de un planeta'), nl,
+    write('3. Explicar vida inteligente de un planeta'), nl,
+    write('4. Explicar habitabilidad de un planeta'), nl,
+    write('5. Volver'), nl,
+    write('Selecciona una opcion: (1-5): '),
+    read(Option), explicaciones(Option).
 
 
--- integrador.pl
+++ integrador.pl
@@ -5,7 +5,8 @@ menu :- write('*** Analisis de Vida Inteligente en varios planetas *** \n'),
         write('1. Cargar datos \n'),
         write('2. Imprimir todos los hechos \n'),
         write('3. Realizar consultas \n'),
-        write('4. Salir \n'),
+        write('4. Consulta de explicación \n'),
+        write('5. Salir \n'),
         write('Selecciona una opcion (1-4): '), read(Option), nl,
         execute(Option).
 
@@ -13,6 +14,7 @@ menu :- write('*** Analisis de Vida Inteligente en varios planetas *** \n'),
 execute(1) :- cargar_csvs, menu.
 execute(2) :- imprimir_planetas, nl, menu.
 execute(3) :- menu2, nl.
-execute(4) :- write('Saliendo del programa...'), nl, true.
+execute(4) :- menu_explicaciones, nl.
+execute(5) :- write('Saliendo del programa...'), nl, true. % Cambiar el número de salida
 execute(_) :- write('Opcion no valida.'), nl, menu. %en caso de que ninguno coincida...
 
